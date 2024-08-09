0f1da64e031f48c98c34409b0510a7c164686a6a
rmdtech <rmdishman@outlook.com>
2024-08-09 13:17:05 +0100

    Fixed CSS compatibility with Jest

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,8 @@ module.exports = {
     transform: {
         '^.+\\.jsx?$': 'babel-jest' // Transform JavaScript and JSX files using babel-jest
     },
-    testEnvironment: 'jsdom' // Use jsdom to simulate a browser environment (ideal for React)
+    testEnvironment: 'jsdom', // Use jsdom to simulate a browser environment (ideal for React)
+    moduleNameMapper: {
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy' // Mock CSS imports
+    }
 };
